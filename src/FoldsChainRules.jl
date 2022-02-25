5e671722b97325ef8cd764ec2c76ac2cee4e4ecf
Takafumi Arakaki <aka.tkf@gmail.com>
2022-02-24 23:11:43 -0500

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.25

---
+++ src/FoldsChainRules.jl
@@ -0,0 +1,11 @@
+baremodule FoldsChainRules
+
+module Internal
+
+using ..FoldsChainRules: FoldsChainRules
+
+include("internal.jl")
+
+end  # module Internal
+
+end  # baremodule FoldsChainRules
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using FoldsChainRules
+using Test
+
+@testset "FoldsChainRules.jl" begin
+    # Write your tests here.
+end
